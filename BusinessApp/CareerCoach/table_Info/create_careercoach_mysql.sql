a3d494fbcc431c3d94e9881d34891abee3d2ac79
mfervil <mfervil@yahoo.com>
2014-10-03 12:11:37 -0500

    Removed synched files

--- BusinessApp/CareerCoach/table_Info/create_careercoach_mysql.sql
+++
@@ -1,4 +0,0 @@
-create database careercoach;
-grant usage on *.* to careercoachadmin@localhost identified by 'careercoachadmin';
-grant all privileges on careercoach.* to careercoachadmin@localhost;
-show databases;
--- BusinessApp/workspace/.metadata/.plugins/org.eclipse.core.resources/.history/f2/30e23079e64900141dbcd8249c2f6810
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS springsecuritydemo_db;
-CREATE DATABASE  springsecuritydemo_db CHARACTER SET utf8;
-USE springsecuritydemo_db;
